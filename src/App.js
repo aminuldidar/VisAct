ee0aeec8dd7f03bedc9d20eaa2a6c451ed51dceb
Raihan Uddin <raihan9419@gmail.com>
2023-01-17 13:59:30 +0200

    update functionality and desgin

--- src/App.js
+++ src/App.js
@@ -7,111 +7,136 @@ import BarChart from "./BarChart";
 import ShowData from "./ShowData";
 import GetChartProperties from "./GetChartProperties";
 import FileUpload from "./FileUpload";
+import Navbar from "./component/Navbar";
 
 function App() {
-  
   const [parsedData, setParsedData] = useState([]);
-  
+
   //State to store table Column name
   const [tableRows, setTableRows] = useState([]);
 
   //State to store the values
   const [rowValues, setValues] = useState([]);
   // Track file type
-  const [fileType, setfileType] = useState('test');
+  const [fileType, setfileType] = useState("test");
   const [components, setComponents] = useState([false]);
 
-  const [x_axis, setXAxis] = useState('');
-  const [y_axis, setYAxis] = useState('');
-  const [message, setMessage] = useState('');
-  
+  const [x_axis, setXAxis] = useState("");
+  const [y_axis, setYAxis] = useState("");
+  const [message, setMessage] = useState("");
 
-  const getFileInput = (Props)=>{
+  const getFileInput = (Props) => {
     console.log(Props);
-    
+
     setValues(Props.rowValues);
     setParsedData(Props.parsedData);
-    setTableRows(Props.tableRows); 
-    setfileType(Props.fileType); 
+    setTableRows(Props.tableRows);
+    setfileType(Props.fileType);
     setComponents(Props.components);
+  };
 
-  }
-
-  const getUserInput=(Props)=>{
+  const getUserInput = (Props) => {
     console.log(Props);
     setXAxis(Props.x_axis);
     setYAxis(Props.y_axis);
     setComponents(Props.chatType);
     setMessage(Props.message);
-  }
+  };
 
   return (
     <>
-    <div>
-     
-    <FileUpload onFileLoad={getFileInput}/>
-    <br />
-    <br />
-    <div> 
-      
-      {<h2>{message}</h2>}
-      {/*Block:Line Chart */}
-      {(() => {
-        //console.log(parsedData,components[0]);
-        
-        if(parsedData.length!== 0 && x_axis !=='' && y_axis !=='' && components[0]==='Line Chart'){
-          //console.lognpm start('Inside line chart_1');
-          //setComponents([false]);
-          //console.log(parsedData);
-          return (<LineChart data={{parsedData,  x_axis, y_axis, tableRows, fileType}}/>);
-        }
-      })()}
+      <Navbar />
 
-      {/*Block: Pie Chart */}
+      <FileUpload onFileLoad={getFileInput} />
 
-      {(() => {
-        //console.log(parsedData,components[0]);
-        
-        if(parsedData.length!== 0 && components[0]==='Pie Chart'){
-          //&& x_axis !=='' && y_axis !=='' 
-          //console.lognpm start('Inside line chart_1');
-          //setComponents([false]);
-          console.log(tableRows, rowValues);
-          return (<PieChart chartData={{x_axis, y_axis, tableRows, rowValues, parsedData}}/>);
-        }
-      })()}
+      {tableRows.length > 0 && (
+        <div>
+          <div>
+            {
+              <h4 className="text-center text-sky-500 font-bold text-2xl mt-10">
+                {message}
+              </h4>
+            }
+            {/*Block:Line Chart */}
+            {(() => {
+              //console.log(parsedData,components[0]);
 
+              if (
+                parsedData.length !== 0 &&
+                x_axis !== "" &&
+                y_axis !== "" &&
+                components[0] === "Line Chart"
+              ) {
+                //console.lognpm start('Inside line chart_1');
+                //setComponents([false]);
+                //console.log(parsedData);
+                return (
+                  <LineChart
+                    data={{ parsedData, x_axis, y_axis, tableRows, fileType }}
+                  />
+                );
+              }
+            })()}
 
-      {/*Block: Bar Chart */}
-      
-      {(() => {
-        //console.log(parsedData,components[0]);
-        
-        if(parsedData.length!== 0 && components[0]==='Bar Chart'){
-          //&& x_axis !=='' && y_axis !=='' 
-          //console.lognpm start('Inside line chart_1');
-          //setComponents([false]);
-          console.log(tableRows, rowValues);
-          return (<BarChart chartData={{x_axis, y_axis, tableRows, rowValues, parsedData}}/>);
-        }
-      })()}
+            {/*Block: Pie Chart */}
 
+            {(() => {
+              //console.log(parsedData,components[0]);
 
-    <br />
-    <br />
-      
-    </div>
-      
-      
-    <GetChartProperties onSubmit={{getUserInput,tableRows}}/>
-    </div>
+              if (parsedData.length !== 0 && components[0] === "Pie Chart") {
+                //&& x_axis !=='' && y_axis !==''
+                //console.lognpm start('Inside line chart_1');
+                //setComponents([false]);
+                console.log(tableRows, rowValues);
+                return (
+                  <PieChart
+                    chartData={{
+                      x_axis,
+                      y_axis,
+                      tableRows,
+                      rowValues,
+                      parsedData,
+                    }}
+                  />
+                );
+              }
+            })()}
 
-    <br />
-    <br />
+            {/*Block: Bar Chart */}
 
-    <ShowData tableData={{tableRows, rowValues}}/>
+            {(() => {
+              //console.log(parsedData,components[0]);
+
+              if (parsedData.length !== 0 && components[0] === "Bar Chart") {
+                //&& x_axis !=='' && y_axis !==''
+                //console.lognpm start('Inside line chart_1');
+                //setComponents([false]);
+                console.log(tableRows, rowValues);
+                return (
+                  <BarChart
+                    chartData={{
+                      x_axis,
+                      y_axis,
+                      tableRows,
+                      rowValues,
+                      parsedData,
+                    }}
+                  />
+                );
+              }
+            })()}
+          </div>
+          <div className="flex items-center justify-center gap-10">
+            <div className="">
+              <ShowData tableData={{ tableRows, rowValues }} />
+            </div>
+            <div className="">
+              <GetChartProperties onSubmit={{ getUserInput, tableRows }} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
-
   );
 }
 
--- src/GetChartProperties.js
+++ src/GetChartProperties.js
@@ -1,130 +1,140 @@
 import { useState } from "react";
 
-function GetChartProperties(Props){
-    const [x_axis, setXAxis] = useState('');
-    const [y_axis, setYAxis] = useState('');
-    //const [message, setMessage] = useState('');
-    //const [components, setComponents] = useState([false]);
-    const [chatType, setChatType] = useState([false]);
-    const [dimestions, setDimestions] = useState([]);
+function GetChartProperties(Props) {
+  const [x_axis, setXAxis] = useState("");
+  const [y_axis, setYAxis] = useState("");
+  //const [message, setMessage] = useState('');
+  //const [components, setComponents] = useState([false]);
+  const [chatType, setChatType] = useState([false]);
+  const [dimestions, setDimestions] = useState([]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        //setComponents([true]);
-        //setMessage(`Chart ${x_axis} Vs ${y_axis}!`);
-        //setXAxis('');
-        //setYAxis('');
-        let message=``;
-        if(x_axis!=='' || y_axis!=='')
-            message=`Chart ${x_axis} Vs ${y_axis}!`;
-        else
-            message=`Please select input variables!`
-        Props.onSubmit.getUserInput({x_axis, y_axis, message, chatType});
-      };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //setComponents([true]);
+    //setMessage(`Chart ${x_axis} Vs ${y_axis}!`);
+    //setXAxis('');
+    //setYAxis('');
+    let message = ``;
+    if (x_axis !== "" || y_axis !== "")
+      message = `Chart ${x_axis} Vs ${y_axis}!`;
+    else message = `Please select input variables!`;
+    Props.onSubmit.getUserInput({ x_axis, y_axis, message, chatType });
+  };
 
-    const x_axisSet = (event) =>{
-        console.log(event.target.value);
-        setXAxis(event.target.value)
-        console.log("User Selected Value - ", event.target.value)
+  const x_axisSet = (event) => {
+    console.log(event.target.value);
+    setXAxis(event.target.value);
+    console.log("User Selected Value - ", event.target.value);
+  };
 
-    }
+  const y_axisSet = (event) => {
+    console.log(event.target.value);
+    setYAxis(event.target.value);
+    console.log("User Selected Value - ", event.target.value);
+  };
 
-    const y_axisSet = (event) =>{
-        console.log(event.target.value);
-        setYAxis(event.target.value)
-        console.log("User Selected Value - ", event.target.value)
+  const options = Props.onSubmit.tableRows;
 
-    }
-
-    
-    const options = Props.onSubmit.tableRows;
-   
-    /*
+  /*
      ['AvgTemperature', 'Year', 'Country'];
     const onOptionChangeHandler = (event) => {
         console.log("User Selected Value - ", event.target.value)
     }
     */
-    const chartSelect = ['Line Chart', 'Bar Chart', 'Pie Chart'];
+  const chartSelect = ["Line Chart", "Bar Chart", "Pie Chart"];
 
-    const ChartSelector = (event) => {
-        event.preventDefault();
-        let chType=event.target.value;
-        setChatType([chType]);
+  const ChartSelector = (event) => {
+    event.preventDefault();
+    let chType = event.target.value;
+    setChatType([chType]);
 
-        switch(chType) {
-            case 'Line Chart':
-                setDimestions(['Please choose X Axis','Please choose Y Axis']);
-              break;
-            case 'Bar Chart':
-                setDimestions(['Please choose categories','Please choose value column']);
-              break;
+    switch (chType) {
+      case "Line Chart":
+        setDimestions(["Please choose X Axis", "Please choose Y Axis"]);
+        break;
+      case "Bar Chart":
+        setDimestions([
+          "Please choose categories",
+          "Please choose value column",
+        ]);
+        break;
 
-            case 'Pie Chart':
-                setDimestions(['Please choose Pie chat categories','Please choose corresponding value column']);
-                break;
-            default:
-              // code block
-          }
-        
-        //setComponents([event.target.value])
-        //setChatType([chType]);
-        
-        console.log("User Selected Value - ", event.target.value)
+      case "Pie Chart":
+        setDimestions([
+          "Please choose Pie chat categories",
+          "Please choose corresponding value column",
+        ]);
+        break;
+      default:
+      // code block
     }
 
-    //console.log(Props.onSubmit.tableRows);
+    //setComponents([event.target.value])
+    //setChatType([chType]);
 
-return(<>
-    <h3>Provide Variable Names (X Axis and Y Axis)</h3>
-    <form onSubmit={handleSubmit}>
-        
-            <select onChange={ChartSelector}>
-                <option>Please choose Chart</option>
-                {chartSelect.map((option, index) => {
-                    return <option key={index} >
-                        {option}
-                    </option>
-                })}
-            </select>
-            <br/>
-            <br/>
-            {(() => {
-                if(chatType[0]!==false){
-                    return (<>
-                        <select onChange={x_axisSet}>
-                        <option>{dimestions[0]}</option>
-                        {options.map((option, index) => {
-                            return <option key={index} >
-                                {option}
-                            </option>
-                        })}
-                        </select>
+    console.log("User Selected Value - ", event.target.value);
+  };
 
-                        <br/>
-                        <br/>
+  //console.log(Props.onSubmit.tableRows);
 
-                        <select onChange={y_axisSet}>
-                        <option>{dimestions[1]}</option>
-                        {options.map((option, index) => {
-                            return <option key={index} >
-                                {option}
-                            </option>
-                        })}
-                        </select>
-                        </>);
-                }
+  return (
+    <>
+      <h3 className="text-lg">Select chart for getting visualize data</h3>
+      <form onSubmit={handleSubmit}>
+        <select
+          onChange={ChartSelector}
+          className="bg-sky-500 p-[14px] border rounded-md  text-medium text-white hover:bg-cyan-500 cursor-pointer"
+        >
+          <option>Please choose Chart</option>
+          {chartSelect.map((option, index) => {
+            return <option key={index}>{option}</option>;
+          })}
+        </select>
+        <br />
+        <br />
+        {(() => {
+          if (chatType[0] !== false) {
+            return (
+              <>
+                <h3 className="text-lg">
+                  Provide Variable Names (X Axis and Y Axis)
+                </h3>
+                <select
+                  onChange={x_axisSet}
+                  className="bg-teal-500 p-[14px] border rounded-md text-medium text-white hover:bg-cyan-500 cursor-pointer"
+                >
+                  <option>{dimestions[0]}</option>
+                  {options.map((option, index) => {
+                    return <option key={index}>{option}</option>;
+                  })}
+                </select>
 
-            })()}
+                <br />
+                <br />
 
-        <br/>
-        <br/>
-      
-        <button type="submit">Show Chart</button>
-    </form>
-    
-    
-    </>);
+                <select
+                  onChange={y_axisSet}
+                  className="bg-teal-500 p-[14px] border rounded-md text-medium text-white hover:bg-cyan-500 cursor-pointer"
+                >
+                  <option>{dimestions[1]}</option>
+                  {options.map((option, index) => {
+                    return <option key={index}>{option}</option>;
+                  })}
+                </select>
+              </>
+            );
+          }
+        })()}
+
+        <br />
+        <br />
+
+        <button type="submit" className="btn w-[50%]">
+          Show Chart
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default GetChartProperties;
